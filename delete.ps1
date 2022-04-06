06a1774f0f068c01f175e351f361bf53975b8baa
rickjacobo <71619054+rickjacobo@users.noreply.github.com>
2022-04-06 05:56:56 -0700

    Update delete.ps1

--- delete.ps1
+++ delete.ps1
@@ -1,3 +1,8 @@
+param (
+    [parameter(mandatory=$false)]
+    $Id
+)
+
 cd $PSScriptRoot
 
 Function Invoke-SQLQuery {
@@ -28,7 +33,11 @@ Invoke-SQLQuery -Config $Config -Query $Query | Select id,url,keyword | out-file
 rm tempquery.csv
 Write-Host " "
 
+if ($Id){}
+else {
 $ID = Read-Host "Select the service 'id' you would like to delete"
+}
+
 if ($ID) {
 $Query = "Delete FROM [TABLE] WHERE id=$ID;"
 $Query = $Query.Replace("[TABLE]","$Table")
